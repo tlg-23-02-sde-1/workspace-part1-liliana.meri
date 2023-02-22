aef88d0b529790eb1f29e4ef9a90edaab56178fd
Liliana Meriweather <liliana.meriweather@tlgcohort.com>
2023-02-22 08:45:04 -0800

    Lab 5.2 add static imports

--- Lab05.2/src/DerivedDateTimeTest.java
+++ Lab05.2/src/DerivedDateTimeTest.java
@@ -12,11 +12,19 @@
  * For the purposes of this lab, we give you a few wildcarded imports,
  * so you can use the classes in these packages without having to worry about importing them.
  */
-import java.time.*;
-import java.time.format.*;
-import java.time.temporal.TemporalAdjusters;
+
+//import java.time.*;
+//import java.time.format.*;
+//import java.time.temporal.TemporalAdjusters;
 
 import static java.time.temporal.TemporalAdjusters.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import static java.time.DayOfWeek.*; // now you can say MONDAY instead of DayOfWeek
+import static java.time.temporal.TemporalAdjusters.*;
+
 
 class DerivedDateTimeTest {
 
@@ -30,13 +38,13 @@ class DerivedDateTimeTest {
         //testEarlyRetirement();
         //testLaborDay();
         // testElectionDay();
-         testAnniversary();
+        testAnniversary();
     }
 
     /**
      * TASK: new American presidents are often measured by their performance during the first 100 days in office.
      * Inauguration Day 2017 is Jan 20.  When is this president's 100-day deadline?
-     *
+     * <p>
      * RESULT:
      */
     public static void testPresidentsFirst100Days() {
@@ -49,7 +57,7 @@ class DerivedDateTimeTest {
      * TASK: Certain times of the year seem to have more birthdays than other times.
      * Some believe that this is because more babies are conceived during certain times of year.
      * Below, assume a due date 38 weeks after conception, during a non-leap-year.
-     *
+     * <p>
      * RESULT:
      */
     public static void testPopularBirthdays() {
@@ -69,7 +77,7 @@ class DerivedDateTimeTest {
      * TASK: you've saved diligently your whole life and plan to retire as soon as
      * you can take distributions from your 401(k) penalty-free, which is when you turn 59 1/2.
      * When will / did you retire?
-     *
+     * <p>
      * RESULT:
      */
     public static void testEarlyRetirement() {
@@ -84,25 +92,26 @@ class DerivedDateTimeTest {
      * TASK: when was Labor Day during the year you were born?
      * Note: start with a LocalDate equal to your birthday.
      * Note: Labor Day is the first Monday in Sept.
-     *
+     * <p>
      * RESULT:
      */
     public static void testLaborDay() {
         LocalDate sept1 = LocalDate.of(1995, 3, 10);
-        LocalDate laborDay = sept1.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
+        LocalDate laborDay = sept1.with(nextOrSame(MONDAY));
         System.out.println(laborDay);
+        System.out.println(laborDay.getDayOfWeek());
 
     }
 
     /**
      * TASK: Election Day in the United States is defined as the Tuesday immediately after the 1st Monday in November.
      * When is Election Day 2024?
-     *
+     * <p>
      * RESULT:
      */
     public static void testElectionDay() {
         LocalDate nov1 = LocalDate.of(2024, 11, 1);
-        LocalDate election = nov1.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY)).plusDays(1);
+        LocalDate election = nov1.with(nextOrSame(MONDAY)).plusDays(1);
         System.out.println(election);
         System.out.println(election.getDayOfWeek());
     }
@@ -112,13 +121,13 @@ class DerivedDateTimeTest {
      * They are planning their 50th wedding anniversary, and would like to throw a big party.
      * If their anniversary does not fall on a Saturday, they'd like to have their party the following Saturday.
      * What is the date of the party?
-     *
+     * <p>
      * RESULT:
      */
     public static void testAnniversary() {
         LocalDate wedding = LocalDate.of(1969, 6, 6);
         LocalDate anniversary = wedding.plusYears(50);
-        LocalDate party = anniversary.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY));
+        LocalDate party = anniversary.with(nextOrSame(SATURDAY));
         System.out.println(party);
         System.out.println(party.getDayOfWeek());
 
